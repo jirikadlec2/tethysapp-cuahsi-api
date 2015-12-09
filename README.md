58283013085470b5c0910fdd853132f92db2c1ac
jirikadlec2 <jirikadlec2@gmail.com>
2015-12-08 19:00:30 -0700

    new structure

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# tethysapp-cuahsi-api
-CUAHSI Apps listing for Tethys
+# tethys-list-apps
+List all apps on the Tethys server
+
+Required functionality for the api to list the web apps:
+
+URL: http://apps.hydroshare.org/api/list_apps.html
+
+Return: JSON object including a list of 
+
+    App Name
+    Brief description
+    Min Number of time series used
+    Max number of time series used
+    URL to launch it
+    URL for an icon
---
+++ build/lib.linux-x86_64-2.7/tethysapp/api/controllers.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+
+
+@login_required()
+def home(request):
+    """
+    Controller for the app home page.
+    """
+    context = {}
+    return render(request, 'api/home.html', context)
+
+
+def list_apps_help(request):
+    """
+    Controller for the list_apps_help page.
+    """
+    context = {}
+    return render(request, 'api/list_apps.html', context)
+
+
+def list_apps(request):
+    """
+    Controller for the list_apps page.
+    """
+    app1 = {'name': 'Time Series Viewer',
+            'url': 'http://appsdev.hydroshare.org/apps/timeseries-viewer',
+            'description': 'View graph and descriptive statistics of one time series',
+            'min_series': 1,
+            'max_series': 1,
+            'icon': 'http://appsdev.hydroshare.org/static/timeseries_viewer/images/viewer_icon2.gif'}
+
+    app2 = {'name': 'Time Series Converter',
+            'url': 'http://appsdev.hydroshare.org/apps/ts-converter',
+            'description': 'Convert time series to daily, weekly, monthly, or yearly aggregate',
+            'min_series': 1,
+            'max_series': 1,
+            'icon': 'http://appsdev.hydroshare.org/static/ts_converter/images/hydro.gif',
+            }
+
+    return JsonResponse({"apps":[app1]})
